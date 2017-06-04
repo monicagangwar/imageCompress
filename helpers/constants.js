4220eee2f7244920430ec8eafe80ffea3ac1740d
monicagangwar <monicagangwar10@gmail.com>
2017-06-04 13:39:01 +0530

    added constants

---
+++ helpers/constants.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Constants = {
+    UPLOAD_PATH: __dirname + '/../Utility/',
+    DOWNLOAD_PATH_JPG: __dirname + '/../Utility/jpg/',
+    DOWNLOAD_PATH_PNG: __dirname + '/../Utility/png/',
+    DOWNLOAD_PATH_GIF: __dirname + '/../Utility/gif/',
+    CONTENT_TYPE_JPG: 'image/jpeg',
+    CONTENT_TYPE_PNG: 'image/png',
+    CONTENT_TYPE_GIF: 'image/gif',
+}
+
+module.exports = Constants;
