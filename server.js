6954732f2f718d11717703cc659d32a1ced65afd
monicagangwar <monicagangwar10@gmail.com>
2017-06-26 00:44:43 +0530

    initializing dir for storing tmp files

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const Fs = require('fs');
 const Hapi = require('hapi');
+const Constants = require('./helpers/constants');
 const server = new Hapi.Server();
 
 server.connection({
@@ -17,6 +19,18 @@ server.register([
     }
 
     server.start((err) => {
+        if(!Fs.existsSync(Constants.CSV.DOWNLOAD_PATH)){
+            Fs.mkdirSync(Constants.CSV.DOWNLOAD_PATH);
+        }
+        if(!Fs.existsSync(Constants.JPG.DOWNLOAD_PATH)){
+            Fs.mkdirSync(Constants.JPG.DOWNLOAD_PATH);
+        }
+        if(!Fs.existsSync(Constants.JPEG.DOWNLOAD_PATH)){
+            Fs.mkdirSync(Constants.JPEG.DOWNLOAD_PATH);
+        }
+        if(!Fs.existsSync(Constants.PNG.DOWNLOAD_PATH)){
+            Fs.mkdirSync(Constants.PNG.DOWNLOAD_PATH);
+        }
         console.log('Server running at:', server.info.uri);
     });
 });
